760ea46af333338ab4e0cef0deaaf7414307e67b
Howard Chen <hchen7913@gmail.com>
2026-01-03 15:12:18 -0600

    Re-export the CodePoint module

---
+++ src/Webb/String/CodePoint.purs
@@ -0,0 +1,12 @@
+module Webb.String.CodePoint 
+( module P
+, WCodePoint
+)
+where
+
+import Prelude
+
+import Data.String.CodePoints (CodePoint)
+import Data.String.CodePoints as P
+
+type WCodePoint = CodePoint
